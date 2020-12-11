2884857b95f8ebb5c3210404e7704e8bb6d4849c
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-11 22:23:06 +0100

    Commit for unit10

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Italy";
+let continent = "Europe";
+let population = 60234639;
+
+console.log("country: " + country);
+console.log("continent: " + continent);
+console.log("population: " + population);
