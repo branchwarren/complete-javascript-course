c722f8bb21e54845b8f5b5674a65a858b7939f13
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-12 21:38:21 +0100

    Commit for unit 14

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -8,6 +8,17 @@ change, and which might change?). Then, change these variables to const.
 */
 
 const country = "Italy";
+let continent = "Europe";
 let population = 60234639;
+console.log("population: ", population, "population/2: ", population / 2);
+console.log("population +1: ", population + 1);
 const isIsland = false;
 let language = 'Italian';
+
+finlandPopulation = 6000000;
+console.log("Italy has more inhabitants than Finland? ", population > finlandPopulation);
+averagePopulation = 33000000;
+console.log("Italy has less inhabitants than the average population? ", population < averagePopulation);
+
+const description = country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+console.log(description);
