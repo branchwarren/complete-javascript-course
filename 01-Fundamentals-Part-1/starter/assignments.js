f3ed7a1bf0e839403d5f0893b821c64c35728d8d
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-13 18:37:50 +0100

    Commit for unit22

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,11 +1,22 @@
 /* 
-LECTURE: Taking Decisions: if / else Statements
-1. If your country's population is greater that 33 million, log a string like this to the
-console: 'Portugal's population is above average'. Otherwise, log a string like
-'Portugal's population is 22 million below average' (the 22 is the average of 33
-minus the country's population)
-2. After checking the result, change the population temporarily to 13 and then to
-130. See the different results, and set the population back to original
+LECTURE: Equality Operators: == vs. ===
+1. Declare a variable 'numNeighbours' based on a prompt input like this:
+prompt('How many neighbour countries does your country
+have?');
+2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality
+== for now)
+3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours'
+is greater than 1
+4. Use an else block to log 'No borders' (this block will be executed when
+'numNeighbours' is 0 or any other value)
+5. Test the code with different values of 'numNeighbours', including 1 and 0.
+6. Change == to ===, and test the code again, with the same values of
+'numNeighbours'. Notice what happens when there is exactly 1 border! Why
+is this happening?
+7. Finally, convert 'numNeighbours' to a number, and watch what happens now
+when you input 1
+8. Reflect on why we should use the === operator and type conversion in this
+situation
 */
 
 const country = "Italy";
@@ -26,8 +37,18 @@ const description = country + ' is in ' + continent + ', and its ' + population 
 
 // console.log(`${country} is in ${continent}, and its ${population} people speak ${language}`);
 
-if (population > averagePopulation) {
-    console.log(`${country}'s population is above average`);
+// if (population > averagePopulation) {
+//     console.log(`${country}'s population is above average`);
+// } else {
+//     console.log(`${country}'s population is ${averagePopulation - population} million below average`);
+// }
+
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbours === 1) {
+    console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border');
 } else {
-    console.log(`${country}'s population is ${averagePopulation - population} million below average`);
+    console.log('No borders');
 }
