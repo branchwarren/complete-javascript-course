9b2a9390d40c03b48a78420a2a012602b6a64c61
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-13 23:45:32 +0100

    Commit for unit26

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -64,8 +64,41 @@ change some variables in order to make the condition true (unless you live in
 Canada :D)
 */
 
-if (language === 'English' && population < 50000000 && !isIsland) {
-    console.log(`You should live in ${country} :)`);
-} else {
-    console.log(`${country} does not meet your criteria :(`);
+// if (language === 'English' && population < 50000000 && !isIsland) {
+//     console.log(`You should live in ${country} :)`);
+// } else {
+//     console.log(`${country} does not meet your criteria :(`);
+// }
+
+/* 
+LECTURE: The switch Statement
+1. Use a switch statement to log the following string for the given 'language':
+chinese or mandarin: 'MOST number of native speakers!'
+spanish: '2nd place in number of native speakers'
+english: '3rd place'
+hindi: 'Number 4'
+arabic: '5th most spoken language'
+for all other simply log 'Great language too :D'
+*/
+language = 'arabic';
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+        break;
 }
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,21 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+// if (js === 'amazing') alert('JavaScript is FUN!');
 
 40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
+
+const day = 'monday';
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
