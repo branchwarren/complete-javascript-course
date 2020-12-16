a430585912b2413b1ba485d6265915ed53936c24
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-16 01:01:46 +0100

    commit for unit 34

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,22 +1,35 @@
 'use strict'
 
 /* 
-LECTURE: Functions
-1. Write a function called 'describeCountry' which takes three parameters:
-'country', 'population' and 'capitalCity'. Based on this input, the
-function returns a string with this format: 'Finland has 6 million people and its
-capital city is Helsinki'
-2. Call this function 3 times, with input data for 3 different countries. Store the
-returned values in 3 different variables, and log them to the console
+LECTURE: Function Declarations vs. Expressions
+1. The world population is 7900 million people. Create a function declaration
+called 'percentageOfWorld1' which receives a 'population' value, and
+returns the percentage of the world population that the given population
+represents. For example, China has 1441 million people, so it's about 18.2% of
+the world population
+2. To calculate the percentage, divide the given 'population' value by 7900
+and then multiply by 100
+3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
+store the results into variables, and log them to the console
+4. Create a function expression which does the exact same thing, called
+'percentageOfWorld2', and also call it with 3 country populations (can be
+the same populations)
 */
 
-function describeCountry(country, population, capitalCity) {
-    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+function percentageOfWorld1(population) {
+    return population / 7900 * 100;
 }
 
-const country1 = describeCountry('Italy', 60.36, 'Rome');
-const country2 = describeCountry('Spain', 47.43, 'Madrid');
-const country3 = describeCountry('Germany', 83.02, 'Berlin');
-console.log(country1);
-console.log(country2);
-console.log(country3);
+const percCountry1 = percentageOfWorld1(60.36);
+const percCountry2 = percentageOfWorld1(47.43);
+const percCountry3 = percentageOfWorld1(83.02);
+console.log(percCountry1, percCountry2, percCountry3);
+
+const percentageOfWorld2 = function (population) {
+    return population / 7900 * 100;
+}
+
+const percCountry12 = percentageOfWorld2(60.36);
+const percCountry22 = percentageOfWorld2(47.43);
+const percCountry32 = percentageOfWorld2(83.02);
+console.log(percCountry12, percCountry22, percCountry32);
