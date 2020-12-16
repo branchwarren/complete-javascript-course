efa5a21e1e468cc0a7af819a39295ba9973f7574
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-16 22:05:59 +0100

    commit for unit35

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,19 +1,9 @@
 'use strict'
 
 /* 
-LECTURE: Function Declarations vs. Expressions
-1. The world population is 7900 million people. Create a function declaration
-called 'percentageOfWorld1' which receives a 'population' value, and
-returns the percentage of the world population that the given population
-represents. For example, China has 1441 million people, so it's about 18.2% of
-the world population
-2. To calculate the percentage, divide the given 'population' value by 7900
-and then multiply by 100
-3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
-store the results into variables, and log them to the console
-4. Create a function expression which does the exact same thing, called
-'percentageOfWorld2', and also call it with 3 country populations (can be
-the same populations)
+LECTURE: Arrow Functions
+1. Recreate the last assignment, but this time create an arrow function called
+'percentageOfWorld3'
 */
 
 function percentageOfWorld1(population) {
@@ -33,3 +23,10 @@ const percCountry12 = percentageOfWorld2(60.36);
 const percCountry22 = percentageOfWorld2(47.43);
 const percCountry32 = percentageOfWorld2(83.02);
 console.log(percCountry12, percCountry22, percCountry32);
+
+const percentageOfWorld3 = population => population / 7900 * 100;
+
+const percCountry13 = percentageOfWorld3(60.36);
+const percCountry23 = percentageOfWorld3(47.43);
+const percCountry33 = percentageOfWorld3(83.02);
+console.log(percCountry13, percCountry23, percCountry33);
