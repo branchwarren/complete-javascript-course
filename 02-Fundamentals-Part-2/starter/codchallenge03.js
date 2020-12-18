b822db68de393b1c9ad9be15d319faaca577923b
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-18 23:31:01 +0100

    commit for unit43_1

---
+++ 02-Fundamentals-Part-2/starter/codchallenge03.js
@@ -0,0 +1,10 @@
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    location: 'Portugal',
+    twitter: '@jonasschmedtman',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
