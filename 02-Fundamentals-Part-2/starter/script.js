545cbc056a6e8a0eb3e1f27c9a6a7d03193b084d
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-20 23:54:10 +0100

    commit for unit49

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -173,7 +173,7 @@ LECTURE: Looping Backwards and Loops in Loops
 arrays. Log a string like 'Neighbour: Canada' for each country
 3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't
 worry if it's too difficult for you! But you can still try to figure this out anyway 😉
-*/
+
 const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
 for (let i = 0; i < listOfNeighbours.length; i++) {
     for (let j = 0; j < listOfNeighbours[i].length; j++) {
@@ -181,3 +181,20 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
     }
     console.log('------------------------');
 }
+*/
+
+/* 
+LECTURE: The while Loop
+1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+but this time using a while loop (call the array 'percentages3')
+2. Reflect on what solution you like better for this task: the for loop or the while
+loop?
+*/
+const percentages3 = [];
+let i = 0;
+while (percentages3.length !== percentages.length) {
+    percentages3.push(percentageOfWorld1(populations[i]));
+    i++;
+}
+console.log(percentages3.length === percentages.length ? true : false);
+console.log(percentages3);
