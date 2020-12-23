19eb6eb2f5952091c60c72362e00392cb7416375
Alessandro Pace <pace.alessandro@gmail.com>
2020-12-23 23:50:17 +0100

    commit for unit61

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,23 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const calcAge = birthYear => 2037 - birthYear;
+// const calcAge = birthYear => 2037 - birthYear;
 
-console.log(calcAge(1978));
+// console.log(calcAge(1978));
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'cels',
+    value: Number(prompt('Degrees celsius:')),
+  };
+  console.log(measurement);
+  console.table(measurement);
+
+  console.log(measurement.value);
+  console.warn(measurement.value);
+  console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
