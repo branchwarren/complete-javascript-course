564d63a896ff9bde28f3772d0d78b64463cd1fdb
Alessandro Pace <pace.alessandro@gmail.com>
2021-01-02 00:56:38 +0100

    commit for unit99

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,80 +1,15 @@
 'use strict';
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
 
-// console.log(this); // 'window' object
-
-// const calcAge = function (birthYear) {
-//   console.log(2037 - birthYear);
-//   console.log(this);
-// };
-// calcAge(1991); // undefined
-
-// const calcAgeArrow = birthYear => {
-//   console.log(2037 - birthYear);
-//   console.log(this);
-// };
-// calcAgeArrow(1978); // 'window' object
-
-// const jonas = {
-//   year: 1991,
-//   calcAge: function () {
-//     console.log(this);
-//     console.log(2037 - this.year);
-//   },
-// };
-// jonas.calcAge(); // this = jonas
-
-// const matilda = {
-//   year: 2017,
-// };
-// matilda.calcAge = jonas.calcAge;
-// matilda.calcAge(); // this = matilda
-
-// const f = jonas.calcAge;
-// in this case 'this' = 'undefined', so...
-// f(); script.js:21 Uncaught TypeError: Cannot read property 'year' of undefined
-
-// var firstName = 'Matilda';
-
-const jonas = {
-  firstName: 'Jonas',
-  year: 1991,
-  calcAge: function () {
-    console.log(this);
-    console.log(2037 - this.year);
-
-    // Solution 1
-    // const self = this; // self OR that
-    // const isMillenial = function () {
-    //   console.log(self);
-    //   console.log(self.year >= 1981 && self.year <= 1996);
-    // };
-    // isMillenial();
-
-    // Solution 2
-    const isMillenial = () => {
-      console.log(this);
-      console.log(this.year >= 1981 && this.year <= 1996);
-    };
-    isMillenial();
-  },
-  greet: () => {
-    console.log(this);
-    console.log(`Hey ${this.firstName}`);
-  },
+const me = {
+  name: 'Jonas',
+  age: 30,
 };
-jonas.greet(); // print 'Hey Matilda'
-jonas.calcAge();
-
-// arguments keyword
-const addExpr = function (a, b) {
-  console.log(arguments); // this keyword/object exists always in regular functions
-  return a + b;
-};
-addExpr(2, 5);
-addExpr(2, 5, 8, 12); // it's perfectly legal!
-
-var addArrow = (a, b) => {
-  console.log(arguments); // this gets a 'script.js:77 Uncaught ReferenceError: arguments is not defined'
-  return a + b;
-};
-addArrow(2, 5, 8);
+const friend = me;
+friend.age = 27;
+console.log('Friend: ', friend);
+console.log('Me: ', me);
